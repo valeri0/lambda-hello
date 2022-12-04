46acce7723e27ad9cfed87b75c197c7887ab12ab
valeri0 <mihaila.andrei95@gmail.com>
2022-12-04 11:52:50 +0200

    feat(cdk): implemented draft lambda with layers

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+
+var path = require("path");
 
 export class CdkStack extends cdk.Stack {
+  private readonly LAMBDA_DIRECTORY = path.resolve(__dirname, "../../lambda/");
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const scammer_layer = new lambda.LayerVersion(
+      this,
+      `${this.stackName}-lambda-layer`,
+      {
+        code: lambda.Code.fromAsset(path.join(this.LAMBDA_DIRECTORY, "src")),
+        compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+        description: "Scammer class layer for the hello lambda python",
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambda_function = new lambda.Function(
+      this,
+      `${this.stackName}-lambda-function`,
+      {
+        code: lambda.Code.fromAsset(
+          path.join(this.LAMBDA_DIRECTORY, "index.py")
+        ),
+        handler: "index.lambda_handler",
+        runtime: lambda.Runtime.PYTHON_3_9,
+        layers: [scammer_layer],
+      }
+    );
   }
 }
